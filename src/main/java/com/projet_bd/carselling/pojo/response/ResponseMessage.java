59d74491a6c6d098425a545890e2ce1f64272d0d
patrickfosso49 <patrickfosso49@gmail.com>
2023-06-21 19:45:30 +0100

    creation of pojo classes requests and responses

---
+++ src/main/java/com/projet_bd/carselling/pojo/response/ResponseMessage.java
@@ -0,0 +1,11 @@
+package com.projet_bd.carselling.pojo.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ResponseMessage {
+   
+    private String massage;
+}
